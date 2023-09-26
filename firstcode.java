482949ea91232d2c76a61800ff704fab0b5a2140
Vijay-1718 <146090387+Vijay-1718@users.noreply.github.com>
2023-09-26 17:22:28 +0530

    Update firstcode.java

--- firstcode.java
+++ firstcode.java
@@ -2,6 +2,7 @@ class firstcode
 {
   public static void main (String [] args)
     {
-      System.out.println("Hello")
+      System.out.println("Hello");
+      System.out.println("World");
       }
 }
